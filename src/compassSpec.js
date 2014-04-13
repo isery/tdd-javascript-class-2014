fc97aa7dbbe6fd837549a8ad2d9fe64101cae711
Isery <michael.hettegger@gmx.net>
2014-04-13 16:34:59 +0200

    moved compass class to severate fule

--- src/compassSpec.js
+++ src/compassSpec.js
@@ -1,55 +1,5 @@
-// can't require main.js
-// write code here
+Compass = require('./anim/compass')
 var $ = require('jquery');
-
-function Compass(text, img){
-  this.text = text
-  this.img = img
-  this.counter = 0
-  this.compassMap = {
-    0: 'N',
-    45: 'NE',
-    90: 'E',
-    135: 'SE',
-    180: 'S',
-    225: 'SW',
-    270: 'W',
-    315: 'NW'
-  }
-}
-
-Compass.prototype.getAngle = function(counter) {
-  angle = counter % 360;
-  if(angle < 0){
-    angle = 360 + angle;
-  }
-  return angle;
-}
-
-Compass.prototype.showText = function(text) {
-  $(this.text).html(text)
-}
-
-Compass.prototype.rotate = function(angle) {
-  $(this.img).css('webkitTransform', 'rotate(' + angle + 'deg)');
-}
-
-Compass.prototype.showAndRotate = function(counter) {
-  angle = this.getAngle(counter);
-  this.rotate(angle);
-  if(this.compassMap[angle])
-      angle = this.compassMap[angle];
-  this.showText(angle);
-}
-
-Compass.prototype.mouseWheelHandler = function(event){
-  if(event.wheelDelta > 0){
-    compass.counter++;
-  } else if (event.wheelDelta < 0){
-    compass.counter--;
-  }
-  compass.showAndRotate(compass.counter);
-}
 
 describe('Test suite', function() {
   it('works fine', function() {
