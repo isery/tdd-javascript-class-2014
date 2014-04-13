dcbc27633c1740f6b7e8fa64360490cdf8446014
Isery <michael.hettegger@gmx.net>
2014-04-13 15:23:30 +0200

    refactor + make class

--- src/compassSpec.js
+++ src/compassSpec.js
@@ -2,7 +2,10 @@
 // write code here
 var $ = require('jquery');
 
-var getAngle = function(counter) {
+function Compass(){
+}
+
+Compass.prototype.getAngle = function(counter) {
   angle = counter % 360;
   if(angle < 0){
     angle = 360 + angle;
@@ -14,28 +17,36 @@ describe('Test suite', function() {
   it('works fine', function() {
     expect(true).toBe(true);
   });
-  it('should return 1', function() {
-    expect(getAngle(1)).toBe(1);
-  });
-  it('should return 5', function() {
-    expect(getAngle(5)).toBe(5);
-  });
-  it('should return 180', function() {
-    expect(getAngle(180)).toBe(180);
-  });
-  it('should return 0', function() {
-    expect(getAngle(360)).toBe(0);
-  });
-  it('should return 359', function() {
-    expect(getAngle(-1)).toBe(359);
-  });
-  it('should return 359', function() {
-    expect(getAngle(-180)).toBe(180);
-  });
 });
 
-describe('Test if jquery is working', function() {
-it('works fine', function() {
-    expect($).toBeDefined();
+describe('Test Compass', function() {
+  beforeEach(function() {
+    compass = new Compass();
+  });
+  describe('Test getAngle', function() {
+    it('should return 1', function() {
+      expect(compass.getAngle(1)).toBe(1);
+    });
+    it('should return 5', function() {
+      expect(compass.getAngle(5)).toBe(5);
+    });
+    it('should return 180', function() {
+      expect(compass.getAngle(180)).toBe(180);
+    });
+    it('should return 0', function() {
+      expect(compass.getAngle(360)).toBe(0);
+    });
+    it('should return 359', function() {
+      expect(compass.getAngle(-1)).toBe(359);
+    });
+    it('should return 359', function() {
+      expect(compass.getAngle(-180)).toBe(180);
+    });
+  });
+
+  describe('Test if jquery is working', function() {
+  it('works fine', function() {
+      expect($).toBeDefined();
+    });
   });
 });
