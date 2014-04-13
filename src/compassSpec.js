22979b419e7ba916979bffaae1b0fd25721a1a3b
Isery <michael.hettegger@gmx.net>
2014-04-13 14:59:33 +0200

    testing getAngle

--- src/compassSpec.js
+++ src/compassSpec.js
@@ -1,7 +1,19 @@
-//main = require('./anim/main')
+// can't require main.js
+// write code here
+
+var getAngle = function(counter) {
+  angle = counter % 360;
+  return angle;
+}
 
 describe('Test suite', function() {
   it('works fine', function() {
     expect(true).toBe(true);
+  });
+});
+
+describe('Test getAngle', function() {
+  it('should return 1', function() {
+    expect(getAngle(1)).toBe(1);
   });
 });
